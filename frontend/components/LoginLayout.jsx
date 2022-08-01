7622bfb1ceac99389e026b49d15a2c5bf73feaa2
yasi <zahrafarrokhi2017@gmail.com>
2022-08-01 14:43:11 +0430

    fix

--- frontend/components/LoginLayout.jsx
+++ frontend/components/LoginLayout.jsx
@@ -4,7 +4,7 @@ export default function LoginLayout(props) {
   const { children } = props;
   return (
     <div className="flex bg-background items-center justify-center w-screen h-screen relative">
-      <div className="flex w-[95%] min-h-[85%] md:w-[65%] md:h-[60%] bg-white rounded-t-lg md:rounded-lg shadow-lg absolute mx-auto top-[15%] md:relative ">
+      <div className="flex w-[95%] min-h-[85%] md:min-h-fit md:w-[65%] md:h-[60%] bg-white rounded-t-lg md:rounded-lg shadow-lg absolute mx-auto top-[15%] md:top-0 md:relative ">
         {children}
       </div>
     </div>
