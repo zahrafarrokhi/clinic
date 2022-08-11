d3aea410f74439144234a4043806a95a2f3434c9
yasi <zahrafarrokhi2017@gmail.com>
2022-08-11 16:31:55 +0430

    add simplejwt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    '',
+    'rest_framework',rest_framework_simplejwt
     'authentication',
 ]
 
@@ -125,3 +126,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.User'
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (    
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+ 
+}
