866af461c074402f07049c05180ccc79619af9fb
yasi <zahrafarrokhi2017@gmail.com>
2022-08-11 16:32:29 +0430

    add login view

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -6,6 +6,7 @@ from . import serializers
 from datetime import timedelta, datetime
 from datetime import datetime
 from django.utils import timezone
+from rest_framework.views import APIView
 
 class EmailView(generics.GenericAPIView):
   serializer_class = serializers.EmailSerializer
@@ -23,3 +24,41 @@ class EmailView(generics.GenericAPIView):
     else:
       return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
 
+class Login(APIView):
+    serializer_class = serializers.validateOtpSerializer
+
+    def post(self, request, *args, **kwargs):
+        # serializer = self.serializer_class(
+        #     data=request.data, context={'request': request})
+
+        # if serializer.is_valid():
+        #     user = serializer.validated_data['user']
+
+        #     token_serializer = serializers.TokenObtainPairSerializer()
+        #     token_serializer.user = user
+
+        #     if token_serializer.is_valid():
+        #         return Response(token_serializer.validated_data, status=status.HTTP_200_OK)
+        #     else:
+        #         return Response({'error': 'invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+        # else:
+        #     return Response({'error': 'invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try :
+            serializer = self.serializer_class(
+                data=request.data, context={'request': request})
+
+            serializer.is_valid(raise_exception=True)
+            user = serializer.validated_data['user']
+
+            token_serializer = serializers.TokenObtainPairSerializer()
+            token_serializer.user = user
+
+            token_serializer.is_valid(raise_exception=True)
+            return Response(token_serializer.validated_data, status=status.HTTP_200_OK)
+        except Exception:
+            return Response({'error': 'invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+          
