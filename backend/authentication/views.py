b5be2ffc426da2ba6bed90253fe034ed1e875684
yasi <zahrafarrokhi2017@gmail.com>
2022-08-18 14:23:31 +0430

    add abstract view

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -7,22 +7,53 @@ from datetime import timedelta, datetime
 from datetime import datetime
 from django.utils import timezone
 from rest_framework.views import APIView
+from rest_framework import mixins, viewsets
+from .services import TokenService
 
-class EmailView(generics.GenericAPIView):
+
+class AbstractOtpObtainView(mixins.CreateModelMixin, viewsets.GenericViewSet):
+
+  @property
+  def token_type(self):
+    raise NotImplemented
+
+  def perform_create(self, serializer):
+    user = serializer.validated_data['user']
+    
+    TokenService.send_token(user, serializer.alias_type, self.token_type, **self.MESSAGE_PAYLOAD)
+
+class EmailView(AbstractOtpObtainView):
   serializer_class = serializers.EmailSerializer
+  token_type = OTP.EMAIL
 
-  def post(self, request, *args, **kwargs):
-    serializer = self.serializer_class(data=request.data, context={'request': request})
+  MESSAGE_PAYLOAD = {
+    'template': 'authentication/email_otp.html',
+    'subject': 'Login OTP code'
+  }
+   
+class PhoneView(AbstractOtpObtainView):
+  serializer_class = serializers.PhoneSerializer
+  token_type = OTP.SMS
 
-    if serializer.is_valid():
-      user = serializer.validated_data['user']
-      token = OTP.objects.create(user=user, type=OTP.EMAIL, is_active=True,
-                               exp_date=(timezone.now() +
-                                         timedelta(minutes=2)))
+  MESSAGE_PAYLOAD = {
+    'template': 'template01',
+  }
+
+# class EmailView(generics.GenericAPIView):
+#   serializer_class = serializers.EmailSerializer
+
+#   def post(self, request, *args, **kwargs):
+#     serializer = self.serializer_class(data=request.data, context={'request': request})
+
+#     if serializer.is_valid():
+#       user = serializer.validated_data['user']
+#       token = OTP.objects.create(user=user, type=OTP.EMAIL, is_active=True,
+#                                exp_date=(timezone.now() +
+#                                          timedelta(minutes=2)))
 
      
-    else:
-      return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+#     else:
+#       return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
 
 class Login(APIView):
     serializer_class = serializers.validateOtpSerializer
@@ -30,7 +61,7 @@ class Login(APIView):
     def post(self, request, *args, **kwargs):
         # serializer = self.serializer_class(
         #     data=request.data, context={'request': request})
-
+        # if else
         # if serializer.is_valid():
         #     user = serializer.validated_data['user']
 
@@ -43,7 +74,7 @@ class Login(APIView):
         #         return Response({'error': 'invalid token'}, status=status.HTTP_400_BAD_REQUEST)
         # else:
         #     return Response({'error': 'invalid token'}, status=status.HTTP_400_BAD_REQUEST)
-
+        #  try catch
         try :
             serializer = self.serializer_class(
                 data=request.data, context={'request': request})
