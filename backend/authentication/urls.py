76d86f2f2b006ace6e8cc0fadfb10a7455341882
yasi <zahrafarrokhi2017@gmail.com>
2022-08-18 14:24:23 +0430

    add urls

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path,include
-
+from django.urls import path
+from . import views
 urlpatterns = [
    
-    # path('login/',''))
+    path('login/email/',views.EmailView.as_view()),
+    path('login/sms/',views.PhoneView.as_view())
 ]
