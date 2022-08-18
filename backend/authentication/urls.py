b16ed44621991db3a2f4dc45a3b35bf398e8182e
yasi <zahrafarrokhi2017@gmail.com>
2022-08-18 16:13:22 +0430

    add refresh

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register(r'sms', views.PhoneView, basename='sms')
 router.register(r'email', views.EmailView, basename='email')
 
 urlpatterns = [
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login', include(router.urls)),
 ]
