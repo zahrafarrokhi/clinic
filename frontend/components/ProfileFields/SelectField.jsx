dac59ba545687be0ff3f87db697896caa6772299
yasi <zahrafarrokhi2017@gmail.com>
2022-09-14 16:07:30 +0430

    Add select component

---
+++ frontend/components/ProfileFields/SelectField.jsx
@@ -0,0 +1,26 @@
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+
+
+const SelectField = (props) => {
+  const { value, onChange, label ,options} = props;
+  return (
+    <FormControl
+      // fullWidth
+      className="basis-[30%] flex-grow flex-shrink-0"
+    >
+      <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+    <Select
+      labelId="demo-simple-select-label"
+      id="demo-simple-select"
+      value={value}
+      label={label}
+      onChange={(e)=>onChange(e,e.target.value)}
+    >
+      {options.map((item)=> <MenuItem value={item.id}>{item.name }</MenuItem>)} 
+      
+    </Select>
+  </FormControl>
+  );
+};
+
+export default SelectField;
