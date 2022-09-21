b346a5d48cf3f0141cc712d6ebab4afcc8f6dc9d
yasi <zahrafarrokhi2017@gmail.com>
2022-09-21 16:57:31 +0430

    Add leaflet styles

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -19,6 +19,7 @@ export default class MyDocument extends Document {
 						href=
 "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 					/>
+					<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
 {/* Inject MUI styles first to match with the prepend: true configuration. */}
 					{this.props.emotionStyleTags}
 				</Head>
