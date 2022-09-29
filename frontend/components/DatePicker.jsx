8d8532d69032e9fec1d5e68a72b49230e0f58738
yasi <zahrafarrokhi2017@gmail.com>
2022-09-29 15:00:09 +0330

    Create datepicker

--- frontend/components/DatePicker.jsx
+++ frontend/components/DatePicker.jsx
@@ -1,5 +1,51 @@
-const DatePicker = () => {
-  return ( <></> );
+import React, {useState} from 'react';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import AdapterJalali from '@date-io/date-fns-jalali';
+import { eachDayOfInterval, endOfWeek, format, startOfWeek } from 'date-fns-jalali';
+//
+import { useTheme } from '@emotion/react';
+import { useMediaQuery } from '@mui/material';
+
+class CustomAdapter extends AdapterJalali {
+  getWeekdays = () => {
+    const now = new Date();
+    return eachDayOfInterval({
+      start: startOfWeek(now),
+      end: endOfWeek(now),
+    }).map((val) => format(val, 'EEEE'));
+  }
 }
- 
-export default DatePicker;
+
+export default function ResponsiveDatePickers(props) {
+  const { label, disabled, TextFieldProps, ...others } = props;
+  // const [value, setValue] = useState(new Date());
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'))
+  return (
+    //dateAdapter
+    <LocalizationProvider dateAdapter={CustomAdapter} dateFormats={{
+      monthShort: 'MMMM',
+    }} localeText={{
+      cancelButtonLabel: 'لغو',
+      okButtonLabel: 'تایید',
+    }}>
+   
+        <MobileDatePicker
+        label={label}
+        disabled={disabled}
+          orientation={isMobile?'portrait':'landscape'} 
+          // value={value}
+          // onChange={(newValue) => {
+          //   setValue(newValue);
+          // }}
+          // dayOfWeekFormatter={(day) => day}
+        renderInput={(params) => <TextField {...params} {...TextFieldProps} />}
+        {...others}
+        />
+       
+  
+    </LocalizationProvider>
+  );
+}
--- frontend/pages/patients/new.jsx
+++ frontend/pages/patients/new.jsx
@@ -9,16 +9,14 @@ import { addPatient } from "../../lib/slices/patients";
 import { persianToEnglishDigits } from "../../lib/utils";
 export default function NewPatient() {
   const dispatch = useDispatch();
-  const router = useRouter()
+  const router = useRouter();
   //state
-  const [totalState,setTotalState] = useState(
-    {
-      first_name: "",
-      last_name: "",
-    }
-  )
+  const [totalState, setTotalState] = useState({
+    first_name: "",
+    last_name: "",
+  });
   const [nationalId, setnationalId] = useState();
-    // birthdate
+  // birthdate
   const [birthdate, setBirthDate] = useState();
   //redux
 
@@ -36,72 +34,81 @@ export default function NewPatient() {
       ).unwrap();
       router.push("/patients/profile");
     } catch (e) {
-      console.log(e)
+      console.log(e);
       // setError(true);
     }
   };
-  return(
-  <div className="flex flex-col justify-center items-center w-full pb-6 md:pb-0">
-
-<div className="flex justify-center items-center p-4">
+  return (
+    <div className="flex flex-col justify-center items-center w-full pb-6 md:pb-0">
+      <div className="flex justify-center items-center p-4">
         <div className="text-lg self-center text-center">
-        لطفا کد‌ملی و تاریخ تولد خود را وارد کنید.
-       
+          لطفا کد‌ملی و تاریخ تولد خود را وارد کنید.
         </div>
-    </div>
-      <div className="flex flex-col p-3 my-4 md:my-0 gap-4 items-center">
-      <TextField
-          value={totalState.first_name}
-          onChange={(e)=>setTotalState({...totalState, first_name: e.target.value})}
-        label={"نام"}
-        // placeholder={}
-        InputProps={{
-          sx: {
-            textAlign: 'center',
-          }
-        }}
-        ></TextField>
-  <TextField
-          value={totalState.last_name}
-          onChange={(e)=>setTotalState({...totalState, last_name: e.target.value})}
-        label={" نام خانوادگی"}
-        // placeholder={}
-        InputProps={{
-          sx: {
-            textAlign: 'center',
-          }
-        }}
-        ></TextField>
-    <TextField
-          value={nationalId}
-          onChange={(e)=>setnationalId(e.target.value)}
-        label={"کد‌ملی"}
-        // placeholder={}
-        InputProps={{
-          sx: {
-            textAlign: 'center',
-          }
-        }}
-        ></TextField>
-         <TextField
-          value={birthdate}
-          onChange={e => setBirthDate(e.target.value)}
-        label={"تاریخ تولد"}
-        // placeholder={}
-        InputProps={{
-          sx: {
-            textAlign: 'center',
-          }
-        }}
-        ></TextField>
-        <DatePicker></DatePicker>
       </div>
-      <Button variant="contained" color="primary" className="w-[320px] md:w-[400px] my-4 text-lg"
-       onClick={submit}
-      //  disabled={selectedPatient === null}
-      >ثبت بیمار</Button>
-  </div>)
- }
+      <div className="flex flex-col p-3 my-4 md:my-0 gap-4 items-center">
+        <TextField
+          value={totalState.first_name}
+          onChange={(e) =>
+            setTotalState({ ...totalState, first_name: e.target.value })
+          }
+          label={"نام"}
+          // placeholder={}
+          InputProps={{
+            sx: {
+              textAlign: "center",
+            },
+          }}
+          InputLabelProps={{ shrink: true }}
+        ></TextField>
+        <TextField
+          value={totalState.last_name}
+          onChange={(e) =>
+            setTotalState({ ...totalState, last_name: e.target.value })
+          }
+          label={" نام خانوادگی"}
+          // placeholder={}
+          InputProps={{
+            sx: {
+              textAlign: "center",
+            },
+          }}
+          InputLabelProps={{ shrink: true }}
+        ></TextField>
+        <TextField
+          value={nationalId}
+          onChange={(e) => setnationalId(e.target.value)}
+          label={"کد‌ملی"}
+          // placeholder={}
+          InputProps={{
+            sx: {
+              textAlign: "center",
+            },
+          }}
+          InputLabelProps={{ shrink: true }}
+        ></TextField>
+        <DatePicker
+          value={birthdate}
+          onChange={(value) => setBirthDate(value)}
+          label={"تاریخ تولد"}
+          TextFieldProps={
+            {
+              // helperText: 'test',
+            }
+          }
+        ></DatePicker>
+      </div>
+      <Button
+        variant="contained"
+        color="primary"
+        className="w-[320px] md:w-[400px] my-4 text-lg"
+        onClick={submit}
+        //  disabled={selectedPatient === null}
+      >
+        ثبت بیمار
+      </Button>
+    </div>
+  );
+}
 
 NewPatient.getLayout = (page) => {
   return <LoginLayout>{page}</LoginLayout>;
