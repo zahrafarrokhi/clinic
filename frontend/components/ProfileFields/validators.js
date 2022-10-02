3bac206cd66cef323c79c601b52bd951479c5e6e
yasi <zahrafarrokhi2017@gmail.com>
2022-10-02 17:47:21 +0330

    Add validator docs

--- frontend/components/ProfileFields/validators.js
+++ frontend/components/ProfileFields/validators.js
@@ -1,6 +1,7 @@
 
-
+//  10 => 10 !== null/undefined/0/''  => null , "" => "" === null/undefined/0/'' => 'این فیلد الزامیست'
 export const NotNull = (value) => value ? null : 'این فیلد الزامیست'
+// 10 => funciton('123') => '123'?.length === 10 ? null : `10 رقم وارد کنید`=> `10 رقم وارد کنید`
 export const StringLength = (digits) => (value) => value?.length === digits ? null : `${digits} رقم وارد کنید`
 // export const StringLength10 = (value) => value.length === 10 ? null : `${10} رقم وارد کنید`
 // export const StringLength11 = (value) => value.length === 11 ? null : `${11} رقم وارد کنید`
