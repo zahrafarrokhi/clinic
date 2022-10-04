8696cd605bc1b87f6c9854d8573b9fe3b271cb0d
yasi <zahrafarrokhi2017@gmail.com>
2022-10-04 12:38:04 +0330

    Show empty address when there user has no address

--- frontend/components/ProfileFields/FormRender/AddressList.jsx
+++ frontend/components/ProfileFields/FormRender/AddressList.jsx
@@ -19,10 +19,18 @@ export default function AddressList(props) {
   useEffect (()=>{
     getAddress()
   }, [])
- 
+  //show emty address when there isnt  any address 
+  //1
+  useEffect(() => {
+    if (addresses.length === 0 ){ setShow(true)}
+   
+  }, [addresses])
+  
   return (
     <div className="flex flex-col gap-4">
+      {/* {(addresses.length === 0 || show) && <Address formsTab={formsTab} data={{}} hide={()=>{ setShow(false)} } /> }  */}
       {show && <Address formsTab={formsTab} data={{}} hide={()=>{ setShow(false)} } /> } 
+      {/* {addresses.length === 0 ? <Address formsTab={formsTab} data={{}} hide={()=>{ setShow(false)} } /> : addresses?.map((ad) => <Address formsTab={formsTab } data={ad} key={ad.id} />)} */}
       {addresses?.map((ad) => <Address formsTab={formsTab } data={ad} key={ad.id} />)}
     
      
