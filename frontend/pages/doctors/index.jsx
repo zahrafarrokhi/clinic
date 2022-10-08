1dde171a286ca37c8e432ba42dd6dacbe0c5917b
yasi <zahrafarrokhi2017@gmail.com>
2022-10-08 12:46:53 +0330

    add search and useMemo

--- frontend/pages/doctors/index.jsx
+++ frontend/pages/doctors/index.jsx
@@ -1,15 +1,18 @@
+import { InputAdornment, TextField } from "@mui/material";
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import DoctorItem from "../../components/doctors/DoctorItem";
 import DoctorsTab from "../../components/doctors/DoctorsTab";
 import Navigation from "../../components/navigation/Navigation";
 import { loadDoctors } from "../../lib/slices/doctors";
-
+import SearchIcon from '@mui/icons-material/Search';
 function Doctors() {
   //def state becuse child cant pass props to parent
   const [selectedTab, setSelectedTab] = useState(null);
+  //search
+  const [search,setSearch]=useState('')
 
    //redux
    const doctors = useSelector(state => state.doctorReducer?.doctors)
@@ -22,12 +25,30 @@ function Doctors() {
      }
     
   useEffect(() => { doctorList() }, [])
+
+  const docs = useMemo(() => {
+    return doctors?.
+    filter(doc=> selectedTab ? doc.department.id === selectedTab : true)
+    .filter(dociname=>`${dociname.first_name} ${dociname.last_name}`.includes(search))
+  }, [doctors, selectedTab, search]);
   
   return (
     <div className="mx-auto w-[85%] py-4">
      {/* props */}
       <DoctorsTab selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+       <TextField
+        label="جستجو"
+        value ={search}
+        onChange={(e)=>setSearch(e.target.value)}
+        InputProps={{
+          startAdornment: <InputAdornment position="start"><SearchIcon/></InputAdornment>,
+        }}
+        size="small"
+        className="mt-10"
+
+       />
       <div className="flex flex-wrap gap-2 py-10">
+
         {/* {doctors?.map((doc) => <div> {doc.first_name} {doc.last_name} </div>)} */}
         {/*
           // at the first
@@ -37,7 +58,7 @@ function Doctors() {
           // all departments &  department doctors
           {doctors?.filter(doc=> selectedTab ? doc.department.id === selectedTab : true).map((doc) => <DoctorItem data={doc} /> )}
         */}
-        {doctors?.filter(doc=> selectedTab ? doc.department.id === selectedTab : true).map((doc) => <DoctorItem data={doc} /> )}
+        {docs?.map((doc) => <DoctorItem data={doc} /> )}
         </div>
     
     </div>
