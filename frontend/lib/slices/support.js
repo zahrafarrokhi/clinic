c57c9ad64c4dfa02cb58a7af5856666508097c3b
yasi <zahrafarrokhi2017@gmail.com>
2022-11-12 17:02:38 +0330

    Add create ticket redux

--- frontend/lib/slices/support.js
+++ frontend/lib/slices/support.js
@@ -1,15 +1,16 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from '../axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../axios";
 
-export const IDLE = 'idle';
-export const LOADING = 'loading';
+export const IDLE = "idle";
+export const LOADING = "loading";
 
 export const listTickets = createAsyncThunk(
-  'support/list',
+  "support/list",
   async (data, thunkAPI) => {
-
     try {
-      const response = await axios.get(`/api/support/tickets/`, {params: data});
+      const response = await axios.get(`/api/support/tickets/`, {
+        params: data,
+      });
 
       console.log(response, response.data);
 
@@ -17,22 +18,77 @@ export const listTickets = createAsyncThunk(
     } catch (error) {
       return thunkAPI.rejectWithValue({ error: error.response.data });
     }
-  },
+  }
+);
+//createTicket
+export const createTicket = createAsyncThunk(
+  "support/create",
+  async (data, thunkAPI) => {
+    try {
+      const response = await axios.post(`/api/support/tickets/`, { ...data });
+
+      console.log(response, response.data);
+    
+      return { data: response.data };
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.response.data });
+    }
+  }
 );
 
 
+
+//createMessage
+export const createMessage = createAsyncThunk(
+  "support/createMessage",
+  async (data, thunkAPI) => {
+    try {
+      const fd = new FormData();       
+      fd.append('file', data.file);
+      fd.append('text', data.text);
+      fd.append('ticket', data.ticket);
+      const response = await axios.post(`/api/support/messages/`, fd);
+
+      console.log(response, response.data);
+    
+      return { data: response.data };
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.response.data });
+    }
+  }
+);
+
+//createTicketWithFile
+export const createTicketWithFile = createAsyncThunk(
+  "support/createTicketWithFile",
+  async (data, thunkAPI) => {
+    try {
+      const fd = new FormData();   
+      for (let f of data.files)    
+        fd.append(f.name, f);
+      fd.append('text', data.text);
+      fd.append('subject', data.subject);
+      const response = await axios.post(`/api/support/ticket-create/`, fd);
+
+      console.log(response, response.data);
+    
+      return { data: response.data };
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.response.data });
+    }
+  }
+);
 const internalInitialState = {
   tickets: [],
-  ticket:null,
+  ticket: null,
   error: null,
   loading: IDLE, // false ,not busy
 };
 
 export const ticketSlice = createSlice({
-  name: 'ticket',
+  name: "ticket",
   initialState: internalInitialState,
   reducers: {
-  
     reset: () => internalInitialState,
   },
   extraReducers: (builder) => {
@@ -51,13 +107,67 @@ export const ticketSlice = createSlice({
       //total data => action.payload.data
       //up => {...respose.data } or response.data => action.payload
       state.tickets = action.payload.data;
-      
+
       return state;
     });
 
+    //createTicket
+    builder.addCase(createTicket.pending, (state) => ({
+      ...state,
+      loading: LOADING,
+    }));
+    builder.addCase(createTicket.rejected, (state, action) => ({
+      ...state,
+      loading: IDLE,
+      error: action.payload.error,
+    }));
+    builder.addCase(createTicket.fulfilled, (state, action) => {
+      state.loading = IDLE;
+      //total data => action.payload.data
+      //up => {...respose.data } or response.data => action.payload
+      state.ticket = action.payload.data;
 
+      return state;
+    });
 
+     //createMessage
+     builder.addCase(createMessage.pending, (state) => ({
+      ...state,
+      loading: LOADING,
+    }));
+    builder.addCase(createMessage.rejected, (state, action) => ({
+      ...state,
+      loading: IDLE,
+      error: action.payload.error,
+    }));
+    builder.addCase(createMessage.fulfilled, (state, action) => {
+      state.loading = IDLE;
+      //total data => action.payload.data
+      //up => {...respose.data } or response.data => action.payload
+      // state.tickets = action.payload.data;
+
+      return state;
+    });
+
+    //createTicketWithFile
+    builder.addCase(createTicketWithFile.pending, (state) => ({
+      ...state,
+      loading: LOADING,
+    }));
+    builder.addCase(createTicketWithFile.rejected, (state, action) => ({
+      ...state,
+      loading: IDLE,
+      error: action.payload.error,
+    }));
+    builder.addCase(createTicketWithFile.fulfilled, (state, action) => {
+      state.loading = IDLE;
+      //total data => action.payload.data
+      //up => {...respose.data } or response.data => action.payload
+      // state.tickets = action.payload.data;
+  
+      return state;
+    }); 
   },
 });
 
-export const { reset,} = ticketSlice.actions;
+export const { reset } = ticketSlice.actions;
--- frontend/pages/support/new.jsx
+++ frontend/pages/support/new.jsx
@@ -3,12 +3,67 @@ import React from "react";
 import { useRef } from "react";
 import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
 import Navigation from "../../components/navigation/Navigation";
-
+import { useState } from "react";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { useDispatch } from "react-redux";
+import { createMessage, createTicket, createTicketWithFile } from "../../lib/slices/support";
 export default function NewTicket() {
   const ref = useRef();
+  const [attachment,setAttachment]=useState([])
+  //redux
+  // state
+  const [state,setState]=useState({
+    subject: '',
+    text:''
+  })
+  const dispatch = useDispatch();
+  // const submit = async()=>{
+  //   try {
+      
+  //     const resp = await dispatch(createTicket(
+  //       {subject:state.subject,
+  //        messages:[
+  //         {text:state.text}
+  //        ]
+  //       }
+  //     )).unwrap()
+  //     const id = resp.data.id
 
+  //     for(let f of attachment){
+  //       await dispatch(createMessage(
+  //         {file:f,
+  //          text:"",
+  //          ticket:id
+
+  //         }
+  //       )).unwrap()
+  //     }
+  //   } catch (error) {
+      
+  //   }
+  // }
+  
+  const submit = async()=>{
+    try {
+      
+      const resp = await dispatch(createTicketWithFile(
+        {
+          subject:state.subject,
+        
+          text:state.text,
+          files:attachment,
+         
+        }
+      )).unwrap()
+
+     
+    } catch (error) {
+      
+    }
+  }
+  
   return (
-    <div className="flex flex-col px-8 gap-2">
+    <div className="flex flex-col px-8 gap-2 my-4">
       <div className="flex flex-wrap justify-between mt-4 my-2 items-center ">
         <div className="text-sm text-gray font-bold">پشتیبانی</div>
       </div>
@@ -38,23 +93,38 @@ export default function NewTicket() {
       </div>
 
       <div className="flex flex-col gap-4 max-w-full md:max-w-[600px]">
-        <TextField size="small" label="موضوع"></TextField>
-        <TextField label="پیام" multiline rows={6}></TextField>
+        <TextField size="small" label="موضوع" value={state.subject} onChange={(e)=>{setState({...state,subject: e.target.value })}}></TextField>
+        <TextField label="پیام" multiline rows={6} sx={{
+          '& .MuiInputBase-root': {
+            width: '100% !important',
+          }
+        }} value={state.text} onChange={(e)=>{setState({...state,text:e.target.value})}}></TextField>
+        <div className="flex gap-2 overflow-auto">
+          <input type="file" hidden ref={ref} multiple onChange={(e)=>setAttachment([...attachment,...e.target.files])}/>
+          {attachment.map((item)=>
+          <div className="relative  max-w-full md:w-[400px] md:h-[300px] flex justify-center items-center ">
+            <img src={URL.createObjectURL(item)} alt="" className="max-w-[400px] max-h-[400px] rounded-lg"/>
+            <Button variant="outlined" onClick={()=>setAttachment(attachment.filter(i=>item !== i))}className="absolute left-1 bottom-1 rounded-full aspect-square p-2 min-w-fit" ><DeleteOutlineIcon className="text-xl "/></Button>
+
+          </div>
+          
+          )}
+        </div>
         <div>
-          <input type="file" hidden ref={ref} />
           <Button
             className="flex items-center gap-2"
             color="grayBtn"
             onClick={() => ref.current.click()}
+
           >
             <DriveFolderUploadIcon />
             پیوست
           </Button>
-        </div>
+          </div>
       </div>
 
       <div className="flex flex-row gap-2">
-        <Button size="small" variant="contained">
+        <Button size="small" variant="contained" onClick={submit}>
           ارسال
         </Button>
         <Button size="small" variant="outlined">
