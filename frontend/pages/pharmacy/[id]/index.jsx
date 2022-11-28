01cb5643fd2ab4dd10b5b0a61ac4a889382010fb
yasi <zahrafarrokhi2017@gmail.com>
2022-11-28 18:34:03 +0330

    Add pharmacy images and description

--- frontend/pages/pharmacy/[id]/index.jsx
+++ frontend/pages/pharmacy/[id]/index.jsx
@@ -1,4 +1,5 @@
-import { Chip } from "@mui/material";
+import { Button, Chip } from "@mui/material";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useMemo } from "react";
 import { useEffect } from "react";
@@ -6,7 +7,7 @@ import { CgDanger } from "react-icons/cg";
 import { useDispatch, useSelector } from "react-redux";
 import Navigation from "../../../components/navigation/Navigation";
 import { getPrescriptionPatient, getPrescriptionPharmacy } from "../../../lib/slices/pharmacy";
-import { convertStrToJalali } from "../../../lib/utils";
+import { convertStrToJalali, stringifyPrice } from "../../../lib/utils";
 
 const PRESCRIPTION_STATUS_TEXT = {
   waiting_for_response: "در انتظار پاسخ",
@@ -59,7 +60,7 @@ export default function Prescription() {
             ></Chip>
           </div>
         </div>
-        <div className="flex flex-row gap-4">
+        <div className="flex flex-row gap-4 flex-wrap">
           <div className="flex flex-row items-center gap-2 rounded-lg border border-solid border-gray p-1 px-2">
             <div className="text-sm ">تاریخ:</div>
             <div className="text-sm font-bold">
@@ -83,6 +84,51 @@ export default function Prescription() {
       <div className="flex gap-2 items-center text-sm italic">
         <CgDanger />
         کاربر گرامی نسخه شما توسط داروخانه بررسی نشده است. پس از تایید توسط داروخانه هزینه نسخه برای شما پیامک میشود       </div>
+      {prescription?.status != 'waiting_for_response' && <div className="flex flex-col rounded-3xl border-solid border border-gray p-4 gap-12">
+      <div className="before:w-2 before:h-2 before:rounded-full before:bg-primary before:flex font-bold text-primary">
+           نتیجه
+          </div>
+        <div className="justify-between flex flex-row gap-2 flex-wrap">
+          <div className="flex flex-col basis-[40%]">
+          <div className="text-sm text-gray">توضیحات</div>
+          <div className="text-lg font-bold">
+         {prescription?.pharmacy_description}
+          </div>
+          
+
+          </div>
+          <div className="flex flex-col basis-[40%]">
+          <div className="text-sm text-gray">تصاویر پیوست شده</div>
+          <div className="flex gap-2 overflow-auto w-full">{prescription?.pic?.map((p)=><div className="relative aspect-video w-[150px]"><Image layout="fill" src={p.image}/></div>)}</div>
+            </div> 
+        </div>
+        <div className="justify-between flex flex-row gap-2 flex-wrap">
+          <div className="flex gap-4 items-center">
+          <div className="text-sm font-bold">
+            جمع کل:
+          </div>
+          <div className="text-lg font-bold text-primary">
+            {stringifyPrice(prescription?.price)}
+          </div>
+          </div>
+          <div className="flex gap-2 flex-grow md:flex-grow-0">
+          <Button
+                onClick={() => {}}
+                className="flex-grow  basis-[45%] md:flex-grow-0 md:w-32"
+                variant="contained"
+              >
+                ثبت و ارسال
+              </Button>
+              <Button
+                onClick={() => {}}
+                className="flex-grow basis-[45%] md:flex-grow-0 md:w-32"
+                variant="outlined"
+              >
+                انصراف
+              </Button>
+          </div>
+        </div>
+</div>}
     </div>
   );
 }
